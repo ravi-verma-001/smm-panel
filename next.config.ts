1ec11da2f6aba5da5cf4ccb6863421c2448616ed
ravi-verma-001 <72845754+ravi-verma-001@users.noreply.github.com>
2026-02-14 19:16:59 +0530

    new code after vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  output: 'export',
 };
 
 export default nextConfig;
